2d38ba9f4cff351d7822f0ab38ffad9427d0fadf
Henry Ripple <hripple46@gmail.com>
2023-07-03 16:19:44 -0500

    added routes for adding users to groups

--- routers/groups.js
+++ routers/groups.js
@@ -7,9 +7,27 @@ router.get("/:id", async (req, res) => {
   res.json(group);
 });
 
-router.get("/:id/activeusers", async (req, res) => {
-  const group = await Group.findById(req.params.id);
+router.get("/:groupId/activeusers", async (req, res) => {
+  const group = await Group.findById(req.params.groupId);
   res.json(group.activeUsers);
+});
+
+router.post("/:groupId/activeusers/:userId", async (req, res) => {
+  const group = await Group.findById(req.params.groupId);
+  const checkIfExists = group.activeUsers.includes(req.params.userId);
+  if (checkIfExists) {
+    return res.status(400).send("User Already Belongs to Group");
+  }
+  const index = group.pendingUsers.indexOf(req.params.userId);
+  group.pendingUsers.splice(index, 1);
+  await group.activeUsers.push(req.params.userId);
+  await group.save();
+  res.json(group.activeUsers);
+});
+
+router.get("/:groupId/pendingusers", async (req, res) => {
+  const group = await Group.findById(req.params.groupId);
+  return res.json(group.pendingUsers);
 });
 
 module.exports = router;
